92663788c345663f8230bc4188e87e771f9d0e0c
ceubie <cras.ceubanks@gmail.com>
2024-08-01 15:28:02 -0400

    edits

--- homework.js
+++ homework.js
@@ -7,14 +7,14 @@ studentAge = 20;
 studentGrade = "A";
 studentAttendance = "Perfect";
 
-console.log("=======================")                      // Data Types in JS //
+console.log("=======================");                      // Data Types in JS //
 
-console.log(studentName)
-console.log(studentAge)
-console.log(studentGrade)
-console.log(studentAttendance)
+console.log(studentName);
+console.log(studentAge);
+console.log(studentGrade);
+console.log(studentAttendance);
 
-console.log("=======================")
+console.log("=======================");
 
 
 ///////////////////////////////////
@@ -26,30 +26,30 @@ let a = 50;
 let b = 25;
 
 console.log("Sum:", a + b);
-console.log("Difference:", a - b)
-console.log("Product:", a * b)
-console.log("Quotient:", a / b)
+console.log("Difference:", a - b);
+console.log("Product:", a * b);
+console.log("Quotient:", a / b);
 
-console.log("=======================")
+console.log("=======================");
 
 console.log("Updated variable 'a':", a = 100);
 console.log("Updated variable 'b':", b = 50);
 
-console.log("=======================")                       // JavaScript Operators //
+console.log("=======================");                       // JavaScript Operators //
 
 console.log("Is a equal to b?", a == b );
 console.log("Is a greater than b?", a > b);
 console.log("Is a not equal to b?", a != b);
 
-console.log("=======================")
+console.log("=======================");
 
 let isPositive = a > 0 && b > 0
 let isEven = a % 2 == 0 || b % 2 == 0 
 
-console.log("Are both numbers positive?", isPositive)
+console.log("Are both numbers positive?", isPositive);
 console.log("is at least one number even?", isEven);
 
-console.log("=======================")
+console.log("=======================");
 
 
 ///////////////////////////////////
